a127feddc97030df3f780c614ee648ddc5e38837
Edward Yoo <ehyoo97@gmail.com>
2016-06-28 12:47:46 -0400

    able to define graph max/mins and continuing to integrate existing code to new gui

--- GUI_v2/graphs.py
+++ GUI_v2/graphs.py
@@ -2,20 +2,34 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-import matplotlib.pyplot as plt
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvas
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
+from constants import *
+pause_graph = False
 
 class PlotColumn(BoxLayout):
+    # A column consisting of a label and graphs.
+    # label_text: text of label you want on head
+    # width, height: how many graphs wide and tall you want the plot to be
+    # graphs_wanted: number of graphs you want. default width*height
+    # graph_lims: dictionary of x_lim and y_lim. 
+    # template is {graph_#: [(xmin, xmax),(ymin, ymax)]}
     def __init__(self, label_text, 
                 width, height, 
-                graphs_wanted=0, 
+                graphs_wanted=0,
+                graph_lims={}, 
                 *args, **kwargs):
         super(PlotColumn, self).__init__(*args, **kwargs)
         self.orientation = 'vertical'
         self.add_widget(Label(text=label_text, size_hint=(1, 0.05)))
-        self.add_widget(self.figure(width, height, graphs_wanted))
+        self.add_widget(self.figure(width, height, graphs_wanted, graph_lims))
 
-    def figure(self, width, height, graphs_wanted):
+    def figure(self, width, height, graphs_wanted, graph_lims):
+        # Generates a plot with the number of subplots delineated 
+        # by graphs_wanted. If graphs_wanted is not defined, then
+        # generates maximum number of graphs. 
         fig = plt.figure()
         counter = 1
         graph_count = 0
@@ -30,8 +44,66 @@ class PlotColumn(BoxLayout):
                     subplot = fig.add_subplot(height, width, counter)
                     subplot.axis([-10, 10, -1, 1])
                     fig.add_subplot()
+                    for key in graph_lims:
+                        if key == counter:
+                            # recall: [(xmin, xmax), (ymin, ymax)]
+                            subplot.set_xlim(graph_lims[key][0][0], 
+                                graph_lims[key][0][0])
+                            subplot.set_ylim(graph_lims[key][1][0], 
+                                graph_lims[key][1][1])
                     counter += 1
         return FigureCanvas(fig)
+
+    # def figure(self, width, height, graphs_wanted):
+    #     fig = plt.figure(figsize=(21,8), facecolor='#E1E6E8')
+    #     plt.subplots_adjust(left=.3)
+
+    #     # create NUM_PLOTS subplots in 2 rows
+    #     graph_columns = np.ceil(NUM_PLOTS/float(GRAPH_ROWS))
+    #     m_axes = [plt.subplot(int(100 * GRAPH_ROWS + 10 * graph_columns + x + 1)) for x in range(NUM_PLOTS)]
+
+    #     xdata = np.zeros(DATA_SIZE)
+    #     ydata = [np.full(DATA_SIZE, None) for x in range(NUM_PLOTS)]
+
+    #     # set axis limits
+    #     lines = [m_axes[x].plot(ydata[x], '-')[0] for x in range(NUM_PLOTS)]
+    #     for x in range(NUM_PLOTS):
+    #         m_axes[x].set_ylim(-1.2,1.2)
+    #         # m_axes[x].set_title('Plot ' + str(x))
+    #         # m_axes[x].get_xaxis().set_visible(False)
+    #     m_axes[0].set_title('desired_h_xdot (m/s)')
+    #     m_axes[1].set_title('desired_h_ydot (m/s)')
+    #     m_axes[2].set_title('desired_h_rdot (deg/s)')
+    #     m_axes[2].set_ylim(-360,360) 
+
+    #     m_axes[3].set_title('computed_xdot')
+    #     m_axes[4].set_title('computed_ydot')
+    #     m_axes[5].set_title('computed_tdot')
+    #     m_axes[5].set_ylim(-360,360)
+    #     return FigureCanvas(fig)
+
+    # initial state for blitting
+    def init_blit(self):
+        for j in range(NUM_PLOTS):
+            lines[j].set_ydata([])
+            lines[j].set_xdata([])
+            m_axes[j].set_xlim(-10, 0)
+
+        return tuple([line for line in lines])
+
+
+    def update_animation(self, data):
+        global xdata 
+        global ydata
+
+        if pause_graph: return []
+        for i in range(NUM_PLOTS):
+            lines[i].set_xdata(xdata)
+            lines[i].set_ydata(ydata[i])
+
+            xmax = max(xdata)
+            m_axes[i].set_xlim(xmax-10, xmax)
+        return tuple([line for line in lines])
 
 class Layout(BoxLayout):
     def __init__(self, **kwargs):
---
+++ live_plot/hamr_live/callbacks.py
@@ -0,0 +1,218 @@
+#######################################################
+# CALLBACK FUNCTIONS
+#######################################################
+def callback_connect(event):
+    if button_connect.label.get_text() == 'Connect':
+        connect_arduino()
+    else:
+        disconnect_arduino()
+
+
+def callback_log_data(event):
+    if not device.is_open:
+        print "You're not connected\n"
+        return
+
+    if button_log_data.label.get_text() == 'Log Data':
+        log_arduino()
+    else:
+        end_log_arduino()
+
+
+def callback_pause_graph(event):
+    if not device.is_open:
+        print "You're not connected\n"
+        return
+
+    global pause_graph
+    if button_pause_graph.label.get_text() == 'Pause Graph':
+        pause_graph = True
+        button_pause_graph.label.set_text('Unpause Graph')
+    else:
+        pause_graph = False
+        button_pause_graph.label.set_text('Pause Graph')
+
+
+def callback_update_arduino(event):
+    if not device.is_open: return
+    write_r_motor(slider_input_1.val)
+    time.sleep(WRITE_DELAY)
+    # write_l_motor(slider_input_2.val)
+    # time.sleep(WRITE_DELAY)
+    write_t_motor(slider_input_3.val)
+    time.sleep(WRITE_DELAY)
+    write_P(slider_P.val)
+    time.sleep(WRITE_DELAY)
+    write_I(slider_I.val)
+    time.sleep(WRITE_DELAY)
+    write_D(slider_D.val)
+    time.sleep(WRITE_DELAY)
+
+def callback_reset_arduino(event):
+    write_r_motor(0)
+    slider_input_1.set_val(0)
+    time.sleep(WRITE_DELAY)
+    write_l_motor(0)
+    # slider_input_2.set_val(0)
+    # time.sleep(WRITE_DELAY)
+    write_t_motor(0)
+    slider_input_3.set_val(0)
+    time.sleep(WRITE_DELAY)
+
+
+update_arduino_immediately = False
+def callback_check(label):
+    global update_arduino_immediately
+    update_arduino_immediately = not update_arduino_immediately
+
+
+def write_r_motor(val):
+    # global r_motor_prev
+    val = precision(val, .1)
+    # if r_motor_prev  != val:
+    device.write(b'r')
+    device.write(str(val))
+    # r_motor_prev = val
+
+def write_l_motor(val):
+    # global l_motor_prev
+    val = precision(val, .1)
+    # if l_motor_prev  != val:
+    device.write(b'l')
+    device.write(str(val))
+    # l_motor_prev = val
+
+def write_t_motor(val):
+    # global t_motor_prev
+    val = precision(val, .1)
+    # if t_motor_prev  != val:
+    device.write(b't')
+    device.write(str(val))
+    # t_motor_prev = val
+
+input_motors = [SIG_R_MOTOR, SIG_L_MOTOR, SIG_T_MOTOR]
+input_holonomic = [SIG_HOLO_X, SIG_HOLO_Y, SIG_HOLO_R]
+input_dd = [SIG_DD_V, SIG_DD_R, b'^'] # the third signal is a placeholder. it should never be used
+current_input = input_holonomic
+
+def write_input_1(val):
+    print current_input[0]
+    print str(val)
+    val = precision(val, INPUT_PRECISION)
+    device.write(current_input[0])
+    device.write(str(val))
+
+def write_input_2(val):
+    print current_input[1]
+    print str(val)
+    val = precision(val, INPUT_PRECISION)
+    device.write(current_input[1])
+    device.write(str(val))
+
+def write_input_3(val):
+    print current_input[2]
+    print str(val)
+    val = precision(val, INPUT_PRECISION)
+    device.write(current_input[2])
+    device.write(str(val))
+
+
+
+
+
+
+# map labels to control signals
+current_mode = 'HOLO X'
+P_map = {'R Motor': SIG_R_KP, 'L Motor': SIG_L_KP, 'T Motor': SIG_T_KP, 'HOLO X': SIG_HOLO_X_KP, 'HOLO Y': SIG_HOLO_Y_KP, 'HOLO R': SIG_HOLO_R_KP}
+I_map = {'R Motor': SIG_R_KI, 'L Motor': SIG_L_KI, 'T Motor': SIG_T_KI, 'HOLO X': SIG_HOLO_X_KI, 'HOLO Y': SIG_HOLO_Y_KI, 'HOLO R': SIG_HOLO_R_KI}
+D_map = {'R Motor': SIG_R_KD, 'L Motor': SIG_L_KD, 'T Motor': SIG_T_KD, 'HOLO X': SIG_HOLO_X_KD, 'HOLO Y': SIG_HOLO_Y_KD, 'HOLO R': SIG_HOLO_R_KD}
+
+def write_P(val):
+    # print P_map[current_mode]
+    # print str(val)
+    # print "---------"
+    # print "---------"
+    val = precision(val, PID_PRECISION)
+    device.write(P_map[current_mode])
+    device.write(str(val))
+    time.sleep(WRITE_DELAY)
+
+
+def write_I(val):
+    # print I_map[current_mode]
+    # print str(val)
+    # print "---------"
+    # print "---------"
+    val = precision(val, PID_PRECISION)
+    device.write(I_map[current_mode])
+    device.write(str(val))
+    time.sleep(WRITE_DELAY)
+
+
+def write_D(val):
+    # print D_map[current_mode]
+    # print str(val)
+    # print "---------"
+    # print "---------"
+    val = precision(val, PID_PRECISION)
+    device.write(D_map[current_mode])
+    device.write(str(val))
+    time.sleep(WRITE_DELAY)
+
+
+def callback_slider_input_1(val):
+    if not update_arduino_immediately: return
+    write_input_1(val)
+
+
+def callback_slider_input_2(val):
+    if not update_arduino_immediately: return
+    write_input_2(val)
+
+
+def callback_slider_input_3(val):
+    if not update_arduino_immediately: return
+    write_input_3(val)
+
+
+# PID SLIDERS
+def callback_slider_P(val):
+    if not update_arduino_immediately: return
+    write_P(val)
+
+
+def callback_slider_I(val):
+    if not update_arduino_immediately: return
+    write_I(val)
+
+
+def callback_slider_D(val):
+    if not update_arduino_immediately: return
+    write_D(val)
+
+# RADIO BUTTONS FOR MODE SELECTION
+def callback_radio_mode(label):
+    global current_mode
+    global current_input
+    current_mode = label
+
+    if 'Motor' in label:
+        current_input = input_motors
+        slider_input_1.label.set_text('M1 Velocity')
+        slider_input_2.label.set_text('M2 Velocity')
+        slider_input_3.label.set_text('MT Velocity')
+    elif 'HOLO' in label:
+        current_input = input_holonomic
+        slider_input_1.label.set_text('X dot')
+        slider_input_2.label.set_text('Y dot')
+        slider_input_3.label.set_text('Theta dot')
+    else:
+        current_input = input_dd
+        slider_input_1.label.set_text('Forward V')
+        slider_input_2.label.set_text('Theta dot')
+        slider_input_3.label.set_text('N/A')
+    plt.draw()
+
+
+    # else if 'DD' in label:
+        # current_input = ''
